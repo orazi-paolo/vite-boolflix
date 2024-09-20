f048fde984a8456495033295b0c0efb89547bd83
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-20 15:57:30 +0200

    connect the input to the data to keep track of its value

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,16 +2,27 @@
 export default {
     data() {
         return {
-
+            content: "",
+        }
+    },
+    methods: {
+        search() {
+            // stampo in console per controllare se prendo il valore corretto
+            console.log(this.content);
+            // lo resetto dopo aver fatto la ricerca
+            this.content = "";
         }
     }
 }
 </script>
 
 <template>
-    <h2>
-        Sono la search bar
-    </h2>
+    <header>
+        <nav>
+            <input type="text" v-model="content">
+            <button @click="search()">Search</button>
+        </nav>
+    </header>
 </template>
 
 <style scoped></style>
