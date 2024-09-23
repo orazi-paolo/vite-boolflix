f93740fc23197749ce7901c19e2322d66982b79c
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-23 12:24:06 +0200

    Added stars in place of ‘vote completed MILESTONE 3

--- src/components/MovieLIstCard.vue
+++ src/components/MovieLIstCard.vue
@@ -40,6 +40,11 @@ export default {
                 return `/gb.png`;
             }
             return `/${this.language}.png`;
+        },
+        // creo una computed che mi trasforma il voto in una scala da 1 a 5 per le stelle
+        starRating() {
+            console.log(Math.ceil(this.vote));
+            return Math.ceil((Math.ceil(this.vote) / 10) * 5);
         }
     }
 }
@@ -51,7 +56,12 @@ export default {
         <li>{{ title }}</li>
         <li> {{ originalTitle }}</li>
         <li><img class="language" :src="flag" :alt="language" @error="imgError"></li>
-        <li>{{ vote }}</li>
+        <li>
+            <span v-for="n in 5" :key="n">
+                <i v-if="n <= starRating" class="fas fa-star"></i>
+                <i v-else class="far fa-star"></i>
+            </span>
+        </li>
 
     </ul>
 </template>
